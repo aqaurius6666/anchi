c277245d967fc4e74dac2ca683cba6d2e7acad92
Nguyen Thinh <ng.tr.thinh@gmail.com>
2022-05-19 02:31:36 +0700

    add 2 restaurants

--- src/redux/initialData/foodData.js
+++ src/redux/initialData/foodData.js
@@ -5,7 +5,7 @@ const data = [
     tags: [1, 2],
     ingredients: [101, 301, 501],
     description: 'Bánh này ngonnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn',
-    address: ['địa chỉ 1', 'địa chỉ 2'],
+    restaurant: ['địa chỉ 1', 'địa chỉ 2'],
   },
   {
     id: 2,
@@ -13,7 +13,7 @@ const data = [
     tags: [4, 101],
     ingredients: [102, 302],
     description: 'Bún này ngonnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn',
-    address: ['địa chỉ 1', 'địa chỉ 2'],
+    restaurant: ['địa chỉ 1', 'địa chỉ 2'],
   },
   {
     id: 3,
@@ -22,7 +22,7 @@ const data = [
     ingredients: [101, 202, 801],
     description:
       'Bữa sáng truyền thống với nguyên liệu chủ đạo là trứng. Bao gồm trứng chần (poached egg), thịt nguội, bánh nướng xốp (English muffin) và xốt Hollandaise. Món ăn đơn giản nhưng tinh tế và yêu cầu kỹ năng cao của người đầu bếp',
-    address: ['địa chỉ 1', 'địa chỉ 2'],
+    restaurant: ['địa chỉ 1', 'địa chỉ 2'],
   },
   {
     id: 4,
@@ -31,7 +31,7 @@ const data = [
     ingredients: [301, 101, 203, 701, 601, 702],
     description:
       'Khi thưởng thức, tiramisu có vị thanh ngọt nhẹ nhàng, hơi hơi nồng của mùi rượu, thơm đậm đà mùi cà phê. Sự kết hợp hài hoà này giúp Tiramisu dễ chiếm được cảm tình của nhiều người yêu ẩm thực.',
-    address: ['Paris Gateaux'],
+    restaurant: ['Paris Gateaux'],
   },
   {
     id: 5,
@@ -40,7 +40,7 @@ const data = [
     ingredients: [301, 101, 703, 701, 202, 704],
     description:
       'Một chút đắng nhẹ từ socola, một chút man mát từ kem tươi, vị ngọt ngào từ gato hạnh nhân…hương vị đặc biệt này chắc chắn sẽ chinh phục được gu thưởng thức của khách hàng, kể cả những thực khách khó tính.',
-    address: ['Paris Gateaux'],
+    restaurant: ['Paris Gateaux'],
   },
   {
     id: 6,
@@ -49,7 +49,7 @@ const data = [
     ingredients: [101, 204, 701, 702],
     description:
       'Dark chocolate and espresso add the slightly bitter notes needed to balance this easy chocolate mousse. Remember, the higher the cacao percentage, the less sweet the chocolate.',
-    address: [],
+    restaurant: [],
   },
   {
     id: 7,
@@ -58,7 +58,7 @@ const data = [
     ingredients: [205, 705, 502],
     description:
       'Panna Cotta is a classic italian dessert made with heavy cream, vanilla and sugar. Gelatin sheets, added to the mixture, create a custard-like consistency.',
-    address: [],
+    restaurant: [],
   },
   {
     id: 8,
@@ -67,7 +67,7 @@ const data = [
     ingredients: [301, 101, 704, 503, 702],
     description:
       'Khéo léo kết hợp với anh đào đen và thạch anh đào tạo nên một chiếc bánh có hương vị ấn tượng, vị kem béo ngậy, ngọt thanh vừa phải, vị thạch anh đào thơm ngon, dai giòn. Lớp xung quanh bánh là socola bào được phủ kín, nhìn rất lạ mắt và kích thích vị giác.',
-    address: ['Paris Gateaux'],
+    restaurant: ['Paris Gateaux'],
   },
 ];
 
--- src/redux/initialData/restaurantData.js
+++ src/redux/initialData/restaurantData.js
@@ -17,8 +17,37 @@ const data = [
     title: 'SG Quán',
     tags: [1, 3],
     address: '21 Cầu Giấy, Hà Nội',
-    describe: 'Quán này ngonnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn',
+    description: 'Quán này ngonnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnnn',
     menu: ['Bánh táo', 'Bánh Chuối'],
+    note: {
+      'Ăn tại quán': true,
+      'Hút thuốc': false,
+    },
+  },
+  {
+    id: 3,
+    title: 'Paris Gateaux Bakery & Cafe',
+    tags: [1, 3, 102, 103, 104],
+    address: '68 Vũ Phạm Hàm, Trung Hoà, Cầu Giấy, Hà Nội',
+    description: 'Paris Gateaux',
+    menu: ['Tiramisu', 'Panna Cotta', 'Mousse', 'Black Forest'],
+    note: {
+      'Ăn tại quán': true,
+      'Hút thuốc': false,
+    },
+  },
+  {
+    id: 4,
+    title: 'Artemis Pastry & Coffee Shop',
+    tags: [1],
+    address: '20 P. Ngô Quyền, Tràng Tiền, Hoàn Kiếm, Hà Nội',
+    description: 'Artemis Pastry',
+    menu: [
+      'Bailey Caramel Brownie',
+      'Burnt Caramel Signature',
+      'Creamy Choco',
+      'Berry Fruit Vanilla',
+    ],
     note: {
       'Ăn tại quán': true,
       'Hút thuốc': false,
